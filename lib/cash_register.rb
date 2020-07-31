a58c6767138cb70e2a684c5da64226c5e3776a82
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-31 21:38:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,12 @@
 
+class CashRegister
+  
+  attr_accessible :cash_register :cash_register_with_discount
+  
+  def initialize(total=0)
+    @total=total
+  end
+  
 
 
 
@@ -12,3 +20,6 @@
 
 
 
+describe 'CashRegister' do
+  let(:cash_register) { CashRegister.new }
+  let(:cash_register_with_discount) { CashRegister.new(20) }
