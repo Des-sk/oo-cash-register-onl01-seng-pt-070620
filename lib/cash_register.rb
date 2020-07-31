87aca898c7ee826b4eff8e31234cedb4095bfcc4
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-31 21:59:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,10 +4,10 @@ class CashRegister
   attr_accessor :total, :discount, :last_transaction_amount, :items
 
   
-  def initialize(total = 0)
-    @total = total
-    @discount = 20
-    @item = [ ]
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
   
 end
