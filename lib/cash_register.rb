a68de62dbf38c4866a734b1a2b61e462dc9868d4
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-01 00:59:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
     if @discount != 0 
       "After the discount, the total comes to $#{@total}."
     else @discount = 0 
-      
+      "There is no discount to apply."
       
       
     end
