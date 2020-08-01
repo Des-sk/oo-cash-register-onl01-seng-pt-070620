8bb24024b2b85946bd95b9be6ee98eeff7ae3635
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-01 00:38:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,13 +11,6 @@ class CashRegister
   end
   
   def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
       @items << title
     end
     @total += price*quantity
