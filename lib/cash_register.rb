cb5127ed242e4632028d17ed96f9fcaffa9e056f
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-01 00:54:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
   def apply_discount
     @ltotal -= (@total * @discount.to_f / 100).to_i
     if @discount != 0 
-      
+      "After the discount, the total comes to #{
   
   
   
